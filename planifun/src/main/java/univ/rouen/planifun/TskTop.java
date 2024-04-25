53d6f4d078d276cfae118a177368e5c5cdf3ba9b
Izana <Izana.khabouri>
2024-04-25 23:01:20 +0200

    add console part

--- planifun/src/main/java/univ/rouen/planifun/TskTop.java
+++ planifun/src/main/java/univ/rouen/planifun/TskTop.java
@@ -6,29 +6,30 @@ import univ.rouen.planifun.app.analyzer.Analyze;
 import univ.rouen.planifun.app.builder.xml.read.ReadingXML;
 
 public class TskTop {
-    public static void main(String[] args) {
-        try {
-            ReadingXML reader = new ReadingXML(new File("C:\\Users\\Izaalala\\Documents\\save_example.xml"));
-            reader.readFileXML();
-            Analyze analyze = new Analyze(reader.getSetTaskInFile());
-            analyze.analyze();
-            analyze.getAllTheMostUrgentTask();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
-        /*if (args.length != 1) {
+    public static void main(String[] args) {
+        if (args.length != 1) {
             System.out.println("Erreur de syntaxe");
         } else {
             try {
-                ReadingXML reader = new ReadingXML(new File(args[0]));
-                Analyze analyze = new Analyze(reader.getSetTaskInFile());
-                analyze.getAllTheMostUrgentTask();
+                
+                String filePath = args[0];
+                File file = new File(filePath);
+                
+                if (file.exists()) {
+                    ReadingXML reader = new ReadingXML(file);
+                    reader.readFileXML();
+                    Analyze analyze = new Analyze(reader.getSetTaskInFile());
+                    analyze.analyze();
+                    analyze.getAllTheMostUrgentTask();
+                } else {
+                    System.out.println("Le fichier n'existe pas : " + filePath);
+                }
             } catch (IOException e) {
-                System.out.println("Erreur de lecture");
+                System.out.println("Erreur de lecture du fichier");
             }
-
             
-        }*/
+        }
     }
+    
 }

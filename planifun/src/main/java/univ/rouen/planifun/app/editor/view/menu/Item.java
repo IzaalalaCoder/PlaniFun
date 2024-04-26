b86191949a23fd22ae0ff0eaa75912a89d783014
Izana <Izana.khabouri>
2024-04-26 13:27:27 +0200

    add item for add task

--- planifun/src/main/java/univ/rouen/planifun/app/editor/view/menu/Item.java
+++ planifun/src/main/java/univ/rouen/planifun/app/editor/view/menu/Item.java
@@ -1,9 +1,11 @@
 package univ.rouen.planifun.app.editor.view.menu;
 
 public enum Item {
+    ADD("Ajouter une tâche"),
+    SEP_ONE(null),
     LOAD("Charger une liste"),
     SAVE("Sauvegarder une liste"),
-    SEP(null),
+    SEP_TWO(null),
     QUIT("Quitter l'application");    
     
     private String title;

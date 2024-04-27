6b3c0a677e57d68d32a1fe8291cedb479db6555a
Izana <Izana.khabouri>
2024-04-27 16:31:36 +0200

    add look

--- planifun/src/main/java/univ/rouen/planifun/TskEdit.java
+++ planifun/src/main/java/univ/rouen/planifun/TskEdit.java
@@ -1,12 +1,20 @@
 package univ.rouen.planifun;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
-import javax.swing.JFrame;
 import univ.rouen.planifun.app.editor.view.EditorMain;
 
 public class TskEdit {
     public static void main(String[] args) {
-        JFrame frame = new EditorMain();
-        frame.setVisible(true);
+        try {
+            // Utiliser le look and feel Nimbus
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+            // Créer et afficher votre fenêtre
+            EditorMain editorMain = new EditorMain();
+            editorMain.setVisible(true);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
     }
 }

ffc75ec2731e0e1c2f8cb9abc2bf1ef7afac3318
Izana <Izana.khabouri>
2024-04-28 00:28:35 +0200

    fix set default date

--- planifun/src/main/java/univ/rouen/planifun/app/editor/model/task/basic/BasicTask.java
+++ planifun/src/main/java/univ/rouen/planifun/app/editor/model/task/basic/BasicTask.java
@@ -1,12 +1,8 @@
 package univ.rouen.planifun.app.editor.model.task.basic;
 
-import java.util.Calendar;
-
 public interface BasicTask {
     
     // COMMANDS 
     
     public void setCompletionDate(int completionDate);
-
-    public void setDefaultCalendar(Calendar calendar);
 }

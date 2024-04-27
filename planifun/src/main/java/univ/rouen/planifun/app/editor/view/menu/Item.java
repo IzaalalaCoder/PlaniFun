55be1bc6178817d8f57bfeaa843df825917cc39a
Izana <Izana.khabouri>
2024-04-27 15:28:08 +0200

    add menu

--- planifun/src/main/java/univ/rouen/planifun/app/editor/view/menu/Item.java
+++ planifun/src/main/java/univ/rouen/planifun/app/editor/view/menu/Item.java
@@ -2,6 +2,8 @@ package univ.rouen.planifun.app.editor.view.menu;
 
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
+
 import univ.rouen.planifun.app.editor.controller.menu.ControlCreateTask;
 import univ.rouen.planifun.app.editor.controller.menu.ControlLoadSetTask;
 import univ.rouen.planifun.app.editor.controller.menu.ControlCreateSetTask;
@@ -10,43 +12,55 @@ import univ.rouen.planifun.app.editor.controller.menu.ControlSaveSetTask;
 import univ.rouen.planifun.app.editor.view.EditorMain;
 
 public enum Item {
-    CREATE("Créer une nouvelle liste de tâche") {
+
+    // CONSTANTS
+
+    //private final String PATH_ASSET = "planifun/src/main/resources/assets/";
+
+    // VALUES
+    
+    CREATE("Créer une nouvelle liste de tâche",
+        new ImageIcon(TaskMenu.PATH_ASSET + "add_set_task.png")) {
         @Override
         public ActionListener getEvent(EditorMain main) {
             return new ControlCreateSetTask(main);
         }
     },
-    ADD("Ajouter une tâche") {
+    ADD("Ajouter une tâche",
+        new ImageIcon(TaskMenu.PATH_ASSET + "add_task.png")) {
         @Override
         public ActionListener getEvent(EditorMain main) {
             return new ControlCreateTask(main);
         }
     },
-    SEP_ONE(null) {
+    SEP_ONE(null, null) {
         @Override
         public ActionListener getEvent(EditorMain main) {
             return null;
         }
     },
-    LOAD("Charger une liste") {
+    LOAD("Charger une liste",
+        new ImageIcon(TaskMenu.PATH_ASSET + "load.png")) {
         @Override
         public ActionListener getEvent(EditorMain main) {
             return new ControlLoadSetTask(main);
         }
     },
-    SAVE("Sauvegarder une liste") {
+    SAVE("Sauvegarder une liste",
+        new ImageIcon(TaskMenu.PATH_ASSET + "save.png")) {
         @Override
         public ActionListener getEvent(EditorMain main) {
             return new ControlSaveSetTask(main);
         }
     },
-    SEP_TWO(null) {
+    SEP_TWO(null, null) {
         @Override
         public ActionListener getEvent(EditorMain main) {
             return null;
         }
     },
-    QUIT("Quitter l'application") {
+    QUIT("Quitter l'application",
+        new ImageIcon(TaskMenu.PATH_ASSET + "quit.png")) {
         @Override
         public ActionListener getEvent(EditorMain main) {
             return new ControlQuit(main);
@@ -54,14 +68,20 @@ public enum Item {
     };    
     
     private String title;
+    private ImageIcon img;
 
-    private Item(String t) {
+    private Item(String t, ImageIcon img) {
         this.title = t;
+        this.img = img;
     }
 
     public abstract ActionListener getEvent(EditorMain main);
 
     public String getTitle() {
         return this.title;
+    }
+
+    public ImageIcon getIcon() {
+        return this.img;
     }
 }
---
+++ planifun/src/main/java/univ/rouen/planifun/app/editor/view/menu/TaskMenu.java
@@ -0,0 +1,58 @@
+package univ.rouen.planifun.app.editor.view.menu;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+import univ.rouen.planifun.app.editor.view.EditorMain;
+
+public class TaskMenu extends JMenuBar {
+    
+    // CONSTANTS
+
+    public static final String PATH_ASSET = "planifun/src/main/resources/assets/";
+
+    // ATTRIBUTES
+
+    private EditorMain parent;
+
+    // CONSTRUCTORS
+
+    public TaskMenu(EditorMain parent) {
+        this.parent = parent;
+        this.createMenu();
+    }   
+
+    // REQUESTS
+    // COMMANDS
+    // UTILS
+
+    private void createMenu() {
+        JMenu menu = new JMenu("Gestion");
+        for (Item it : Item.values()) {
+            if (it.getTitle() == null) {
+                menu.addSeparator();
+            } else {
+                JMenuItem item = new JMenuItem(it.getTitle());
+                item.addActionListener(it.getEvent(parent));
+                item.setIcon(this.getIcon(it.getIcon()));
+                menu.add(item);
+            }
+        }
+
+        this.add(menu);
+    }
+
+    private ImageIcon getIcon(ImageIcon imageIcon) {
+        if (imageIcon == null) {
+            return null;
+        }
+        Image image = imageIcon.getImage();
+        Image newimg = image.getScaledInstance(32, 32,  java.awt.Image.SCALE_SMOOTH);
+        return new ImageIcon(newimg);
+    }
+
+}
